e8fbd02df1244558ad3d5ba86a3314a80038bc3a
XiangMin Liu <635750556@qq.com>
2019-05-08 22:23:49 +0800

    refactor(facade): rename facade to proxy

--- Proxy/Validate.php
+++ Proxy/Validate.php
@@ -18,12 +18,12 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Leevel\Validate\Facade;
+namespace Leevel\Validate\Proxy;
 
 use Leevel\Di\Container;
 
 /**
- * 门面 validate.
+ * 代理 validate.
  *
  * @author Xiangmin Liu <635750556@qq.com>
  *
